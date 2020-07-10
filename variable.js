0413ef2ba24c63452014459542c9a178a1633eab
Queenie Villasin <q.villasin@gmail.com>
2020-07-10 20:49:26 +0200

    QA db

--- variable.js
+++ variable.js
@@ -1,4 +1,4 @@
 
-// const api = "https://qa-zypical-api.herokuapp.com/api";
-const api = "http://localhost:3000/api";
+const api = "https://qa-zypical-api.herokuapp.com/api";
+// const api = "http://localhost:3000/api";
 
