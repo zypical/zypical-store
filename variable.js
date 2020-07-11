1ef2b06f532eaff320fb5cad5fd6ea9ea5057efe
Queenie Villasin <q.villasin@gmail.com>
2020-07-11 15:41:40 +0200

    Creation flow

--- variable.js
+++ variable.js
@@ -2,3 +2,26 @@
 const api = "https://qa-zypical-api.herokuapp.com/api";
 // const api = "http://localhost:3000/api";
 
+const goHomeSecondLevel = () => {
+    window.location.replace("../index.html");
+}
+
+const checkSize = (files) => {
+    let correct = true;
+    Object.keys(files).forEach(function(el,index) {
+        let file = files[el];
+        console.log("File checked: ",file.name);
+        if (file.size > 1500000) // 2 mb for bytes.
+        {
+            alert(`File too large for image ${file.name}. Please upload equal or less than 1.5MB`)
+            console.log("Image size: ", file.size);
+            correct =  false;
+        }
+        else {
+
+        }
+    });
+
+    return correct;
+        
+}
